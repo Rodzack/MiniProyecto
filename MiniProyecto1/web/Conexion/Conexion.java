d979f2543aa30859a01c6d9f25615613db5f308c
Juan <>
2015-03-11 08:29:43 -0500

    Se agrego dos java

---
+++ MiniProyecto1/web/Conexion/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class Conexion {
+   /* try
+      {
+         Class.forName("com.mysql.jdbc.Driver");
+         Connection conex = DriverManager.getConnection(
+            "jdbc:mysql://localhost:83/PruebaBD", "root", "");
+         Statement st = conex.createStatement();
+      catch(excepction e){
+    
+        }*/
+}
