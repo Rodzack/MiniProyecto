a51d2f8809d06c378d26b6f907c66ddfa928a95f
JuanD <>
2015-03-11 09:17:28 -0500

    Se agrego los get y set

--- MiniProyecto1/web/Conexion/Beans.java
+++ MiniProyecto1/web/Conexion/Beans.java
@@ -22,6 +22,86 @@ public class Beans {
     //Variables de producto
     private String nombreProducto;
     private String idTipoProducto;
+
+    public String getCapacidadmM() {
+        return CapacidadmM;
+    }
+
+    public void setCapacidadmM(String CapacidadmM) {
+        this.CapacidadmM = CapacidadmM;
+    }
+
+    public String getVelocidad() {
+        return Velocidad;
+    }
+
+    public void setVelocidad(String Velocidad) {
+        this.Velocidad = Velocidad;
+    }
+
+    public String getTipoDeConexion() {
+        return tipoDeConexion;
+    }
+
+    public void setTipoDeConexion(String tipoDeConexion) {
+        this.tipoDeConexion = tipoDeConexion;
+    }
+
+    public String getVoltaje() {
+        return Voltaje;
+    }
+
+    public void setVoltaje(String Voltaje) {
+        this.Voltaje = Voltaje;
+    }
+
+    public String getTamanio() {
+        return Tamanio;
+    }
+
+    public void setTamanio(String Tamanio) {
+        this.Tamanio = Tamanio;
+    }
+
+    public String getDescripcionAdicional() {
+        return descripcionAdicional;
+    }
+
+    public void setDescripcionAdicional(String descripcionAdicional) {
+        this.descripcionAdicional = descripcionAdicional;
+    }
+
+    public String getNombreMarca() {
+        return nombreMarca;
+    }
+
+    public void setNombreMarca(String nombreMarca) {
+        this.nombreMarca = nombreMarca;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getIdTipoProducto() {
+        return idTipoProducto;
+    }
+
+    public void setIdTipoProducto(String idTipoProducto) {
+        this.idTipoProducto = idTipoProducto;
+    }
+
+    public String getTipoProducto() {
+        return tipoProducto;
+    }
+
+    public void setTipoProducto(String tipoProducto) {
+        this.tipoProducto = tipoProducto;
+    }
     //Varaibles TipoProducto
     private String tipoProducto;
 }
